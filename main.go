875bd196f062ec57fc0c399c7e820c5f13bc9c1c
Patten14 <126598504+Patten14@users.noreply.github.com>
2024-05-10 17:49:40 +0200

    Restructured main.go

--- main.go
+++ main.go
@@ -4,34 +4,75 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
+var region string = "us-west-2"
+var bucketName string = "tf-dataengineering-patrick"
+
 func main() {
-	createS3("tf-dataengineering-patrick")
+	createS3(bucketName)
+	ListAllBuckets()
+	// deleteS3(bucketName)
+	// ListAllBuckets()
+
 }
 
-func createS3(bucketName string) {
-	sdkConfig, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
+func GetS3Client() (*s3.Client, error) {
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
 	if err != nil {
 		fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
 		fmt.Println(err)
-		return
+		return &s3.Client{}, err
 	}
-	s3Client := s3.NewFromConfig(sdkConfig)
-	_, erro := s3Client.CreateBucket(context.TODO(),
-		&s3.CreateBucketInput{Bucket: &bucketName,
-			CreateBucketConfiguration: &types.CreateBucketConfiguration{LocationConstraint: types.BucketLocationConstraintUsWest2},
+	return s3.NewFromConfig(sdkConfig), err
+}
+
+func deleteS3(bucketName string) error {
+	s3Client, err := GetS3Client()
+	if err != nil {
+		return err
+	}
+
+	_, err = s3Client.DeleteBucket(context.TODO(),
+		&s3.DeleteBucketInput{Bucket: aws.String(bucketName)})
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Println("Deleted:", bucketName)
+	return nil
+}
+
+func createS3(bucketName string) error {
+	s3Client, err := GetS3Client()
+	if err != nil {
+		return err
+	}
+
+	_, err = s3Client.CreateBucket(context.TODO(),
+		&s3.CreateBucketInput{Bucket: aws.String(bucketName),
+			CreateBucketConfiguration: &types.CreateBucketConfiguration{LocationConstraint: types.BucketLocationConstraint(region)},
 		})
-	if erro != nil {
-		fmt.Print(err)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	fmt.Println("Created:", bucketName)
+	return nil
+}
+
+func ListAllBuckets() {
+	s3Client, err := GetS3Client()
+	if err != nil {
 		return
 	}
 
-	count := 10
-	fmt.Printf("Let's list up to %v buckets for your account.\n", count)
+	fmt.Println("Let's list up all buckets for your account.")
 	result, err := s3Client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
 	if err != nil {
 		fmt.Printf("Couldn't list buckets for your account. Here's why: %v\n", err)
@@ -40,11 +81,13 @@ func createS3(bucketName string) {
 	if len(result.Buckets) == 0 {
 		fmt.Println("You don't have any buckets!")
 	} else {
-		if count > len(result.Buckets) {
-			count = len(result.Buckets)
-		}
-		for _, bucket := range result.Buckets[:count] {
+		for _, bucket := range result.Buckets[:len(result.Buckets)] {
 			fmt.Printf("\t%v\n", *bucket.Name)
 		}
 	}
+}
+
+func UploadFile(bucketName string, input string) error {
+
+	return nil
 }
